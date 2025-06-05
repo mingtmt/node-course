4073336fd70d97289114a31c528073d6fdbd69cd
Tuấn Minh Bùi <btminh.dev@gmail.com>
2025-06-05 20:42:15 +0700

    Feature/custom dynamic mileware for apikey and permissions (#3)
    * hanlde middleware for apikey and permissions
    
    * refactor: remove unused dependencies and clean up code

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const req = require("express/lib/request");
+const { findByKey } = require("../services/apikey.service");
+
+const HEADER = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+
+const apiKey = async (req, res, next) => {
+  try {
+    const key = req.headers[HEADER.API_KEY]?.toString();
+    if (!key) {
+      return res.status(403).json({ message: "Forbidden Error" });
+    }
+
+    // check api key
+    const objKey = await findByKey(key);
+    if (!objKey) {
+      return res.status(403).json({ message: "Forbidden Error" });
+    }
+    req.objKey = objKey;
+
+    return next();
+  } catch (error) {}
+};
+
+const permissions = (permissions) => {
+  return (req, res, next) => {
+    if (!req.objKey.permissions) {
+      return res.status(403).json({ message: "Permission denined" });
+    }
+
+    const validPermissions = req.objKey.permissions.includes(permissions);
+    if (!validPermissions) {
+      return res.status(403).json({ message: "Permission denined" });
+    }
+
+    return next();
+  };
+};
+
+module.exports = {
+  apiKey,
+  permissions,
+};
